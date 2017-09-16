3861b6ba05c3c517b5d3cfc98000eb3a4b562822
andigor <amotsok@gmail.com>
2017-09-16 14:18:12 +0200

    Dump part of server.

--- server_main.cpp
+++ server_main.cpp
@@ -1,4 +1,69 @@
-int main()
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+#include <iostream>
+
+class server
 {
+public:
+  server(boost::asio::io_service& io_service, short port)
+    : io_service_(io_service),
+      acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+  {
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    session* new_session = new session(io_service_);
+    acceptor_.async_accept(new_session->socket(),
+        boost::bind(&server::handle_accept, this, new_session,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(session* new_session,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      new_session->start();
+    }
+    else
+    {
+      delete new_session;
+    }
+
+    start_accept();
+  }
+
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
+
+
+int main(int argc, char** argv)
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    using namespace std; // For atoi.
+    server s(io_service, atoi(argv[1]));
+
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
   return 0;
 }
+
